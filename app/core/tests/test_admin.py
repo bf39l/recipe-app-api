6cc37d33c49966905c1691335ba0f0903e3c6837
Bo Liu <boliu1234@gmail.com>
2020-04-21 04:49:05 +1200

    - Created tests for user edit and user add in admin pages - Added user edit and add pages/fieldsets

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -23,3 +23,16 @@ class AdminSiteTests(TestCase):
     res = self.client.get(url)
     self.assertContains(res, self.user.name)
     self.assertContains(res, self.user.email)
+
+  def test_user_change_page(self):
+    """Test user edit page works"""
+    url = reverse("admin:core_user_change", args=[self.user.id])
+    # /admin/core/user/<user_id>
+    res = self.client.get(url)
+    self.assertEqual(res.status_code, 200)
+
+  def test_create_user_page(self):
+    """Test the create user page works"""
+    url = reverse("admin:core_user_add")
+    res = self.client.get(url)
+    self.assertEqual(res.status_code, 200)
